9f41933db181c5161b868d6de6f236ef13183290
sroehrl <stefan.roehrl@gmail.com>
2020-04-28 13:56:27 -0400

    updates 04/28

--- administrator.php
+++ administrator.php
@@ -51,8 +51,8 @@ function getTestModel($id){
 }
 
 function displayTestForm($testModel){
-    $formHTML = '<form method="post" action="administrator.php?action=update">';
-    $formHTML .= '<input type="hidden" name="test_id" value="'. $testModel['id'].'">';
+    $formHTML = '<form method="post" action="administrator.php?action=update&id='. $testModel['id'].'">';
+    $formHTML .= '<input type="hidden" name="id" value="'. $testModel['id'].'">';
     $formHTML .= '<div class="form-group"><input class="form-input" name="name" value="'.$testModel['name'].'" placeholder="name"></div>';
     foreach ($testModel['questions'] as $i => $question){
         $formHTML .= '<div class="question">';
@@ -63,23 +63,60 @@ function displayTestForm($testModel){
             $markAsChecked = $choice['is_correct'] ? 'checked' : '';
             $formHTML .= '<input class="form-input" name="choice_choice-'.$choice['id'].'" value="'.$choice['choice'].'">';
             $formHTML .= '<input type="checkbox" name="choice_correct-'.$choice['id'].'" '.$markAsChecked.'><br/>';
+            $formHTML .= '<a class="btn btn-error m-2" href="administrator.php?action=delete_choice&choice_id='.$choice['id'].'&id='. $testModel['id'].'" >delete choice</a>';
         }
+        $formHTML .= '<a class="btn btn-success m-2" href="administrator.php?action=add_choice&question_id='.$question['id'].'&id='. $testModel['id'].'" >add choice</a>';
+        $formHTML .= '<p><a class="btn btn-error mt-2" href="administrator.php?action=delete_question&question_id='.$question['id'].'&id='. $testModel['id'].'" >delete question</a></p>';
         $formHTML .= '</div>';
     }
-
+    $formHTML .= '<p><a class="btn btn-success mt-2" href="administrator.php?action=add_question&id='.$testModel['id'].'" >add question</a></p>';
     $formHTML .= '<input type="submit" value="update"></form>';
     return $formHTML;
 }
 
+function editView(){
+    $testModel = getTestModel($_GET['id']);
+    echo displayTestForm($testModel);
+}
 
 if(isset($_GET['action'])){
-    // delete
-    if($_GET['action'] === 'delete'){
-        $connection->query('UPDATE test SET delete_date = NOW() WHERE id ='. (int) $_GET['id']);
-    } elseif($_GET['action'] === 'edit'){
-        $testModel = getTestModel($_GET['id']);
-        echo displayTestForm($testModel);
+    switch ($_GET['action']){
+        case 'delete':
+            // here we delete tests
+            $connection->query('UPDATE test SET delete_date = NOW() WHERE id ='. (int) $_GET['id']);
+            break;
+        case 'edit':
+            // here we show a test to edit
+            editView();
+            break;
+        case 'delete_choice':
+            // here we are going to delete a specific choice & show edit
+            $connection->query('UPDATE choice SET delete_date = NOW() WHERE id ='. (int) $_GET['choice_id']);
+            editView();
+            break;
+        case 'delete_question':
+            // here we are going to delete a specific question & show edit
+            $connection->query('UPDATE question SET delete_date = NOW() WHERE id ='. (int) $_GET['question_id']);
+            editView();
+            break;
+        case 'add_question':
+            // adding a question & showing edit
+            $connection->query('INSERT INTO question SET question = "new question", test_id ='. (int) $_GET['id']);
+            editView();
+            break;
+        case 'add_choice':
+            // add choice to a question
+            $connection->query('INSERT INTO choice SET choice = "answer", question_id ='. (int) $_GET['question_id']);
+            editView();
+            break;
+        case 'update':
+            // update test
+            $connection->query('UPDATE test SET  name = "'.$_POST['name'].'", WHERE id ='. (int) $_POST['id']);
+            // update all questions
+            // update all choices
+            break;
     }
+
 
 }
 
