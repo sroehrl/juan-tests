e077d71c3d9de8867aee963c69e859c87c2e804b
sroehrl <stefan.roehrl@gmail.com>
2020-05-03 14:01:24 -0400

    updates 05/03

--- test.php
+++ test.php
@@ -7,30 +7,30 @@ $connection = $DB->openConnection();
 $testName = 'readcomptest';
 
 
-if(isset($_GET['test'])) {
+if (isset($_GET['test'])) {
     $test = $_GET['test'];
     $testData = $DB->readData('SELECT * FROM test WHERE name = "' . $test . '" AND delete_date IS NULL');
     $testName = $testData[0]['name'];
 }
-    /*
-     * [
-     *      ['id' => 1 , 'name' => 'readcomptest']
-     * ]
-     * */
+/*
+ * [
+ *      ['id' => 1 , 'name' => 'readcomptest']
+ * ]
+ * */
 
-    $questionData = $DB->readData('SELECT * FROM question WHERE test_id = "' . $testData[0]['id'] . '"');
-    $testQuestions = [];
-    foreach ($questionData as $questionDatum){
-        $testQuestions[] = [
-            'question' => $questionDatum,
-            'choices' => $DB->readData('SELECT * FROM choice WHERE question_id = ' . $questionDatum['id'])
-        ];
-    }
-    /*
-     * [
-     *      ['question' => ['id' => 1 , 'question' => 'how are you?'], 'choices' => [...]]
-     * ]
-     * */
+$questionData = $DB->readData('SELECT * FROM question WHERE test_id = "' . $testData[0]['id'] . '"');
+$testQuestions = [];
+foreach ($questionData as $questionDatum) {
+    $testQuestions[] = [
+        'question' => $questionDatum,
+        'choices' => $DB->readData('SELECT * FROM choice WHERE question_id = ' . $questionDatum['id'])
+    ];
+}
+/*
+ * [
+ *      ['question' => ['id' => 1 , 'question' => 'how are you?'], 'choices' => [...]]
+ * ]
+ * */
 
 ?>
 <!DOCTYPE html>
@@ -38,64 +38,72 @@ if(isset($_GET['test'])) {
 <head>
     <meta charset="UTF-8">
     <title><?= $testName ?></title>
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-exp.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-icons.min.css">
 </head>
 <body>
-<form method="post">
-<?php
+<div class="container">
+    <div class="columns">
+        <?php include 'navigation.html' ?>
+        <div class="column">
+            <?php
+
+            // evaluation
+            if (isset($_POST['submission'])) {
+                // '1-1' => string 'on'
+                // insert into database
+                $userId = 1; // $_SESSION['user_id'];
+                $testID = $testData[0]['id'];
+                $connection->query('INSERT INTO result SET test_id = ' . $testID . ', user_id = ' . $userId);
+                $newResultId = $connection->insert_id;
+
+                // all questions
+                $totalQuestions = count($questionData);
+                $correctAnswers = 0;
+
+                foreach ($_POST as $key => $answer) {
+                    if (is_numeric(substr($key, 0, 1)) && $answer) {
+                        $answerParts = explode('-', $key); // [1,1]
+                        $questionId = $answerParts[0];
+                        $choiceId = $answerParts[1];
+                        $connection->query('INSERT INTO result_answer SET result_id = ' . $newResultId . ', choice_id = ' . $choiceId);
+
+                        // correct?
+                        $answerKey = $DB->readData('SELECT is_correct FROM choice WHERE id = ' . $choiceId);
+                        if ($answerKey[0]['is_correct']) {
+                            $correctAnswers++;
+                        }
+
+                    }
+                }
+
+                $score = $correctAnswers / $totalQuestions * 100;
+
+                echo '<h1>YOUR SCORE: ' . $score . '% </h1>';
 
 
-foreach ($testQuestions as $question){
-    echo '<h3>' . nl2br($question['question']['question']) . '</h3>';
-    foreach ($question['choices'] as $choice){
-        // <label><input type="checkbox" name="1-1"/>answer 1</label><br>
-        echo '<label><input type="checkbox" name="'.$choice['question_id'].'-'.$choice['id'].'"/>'.$choice['choice'].'</label><br>';
-    }
-}
-?>
-    <input type="hidden" name="submission" value="test">
-    <button type="submit">submit</button>
-</form>
+                // evaluate results
+            } else { ?>
+            <form method="post">
+                <?php
+
+
+                foreach ($testQuestions as $question) {
+                    echo '<h3>' . nl2br($question['question']['question']) . '</h3>';
+                    foreach ($question['choices'] as $choice) {
+                        // <label><input type="checkbox" name="1-1"/>answer 1</label><br>
+                        echo '<label><input type="checkbox" name="' . $choice['question_id'] . '-' . $choice['id'] . '"/>' . $choice['choice'] . '</label><br>';
+                    }
+                }
+                ?>
+                <input type="hidden" name="submission" value="test">
+                <button type="submit">submit</button>
+            </form>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
-<?php
-
-// evaluation
-if(isset($_POST['submission'])){
-    // '1-1' => string 'on'
-    // insert into database
-    $userId = 1; // $_SESSION['user_id'];
-    $testID = $testData[0]['id'];
-    $connection->query('INSERT INTO result SET test_id = '. $testID . ', user_id = '. $userId);
-    $newResultId = $connection->insert_id;
-
-    // all questions
-    $totalQuestions = count($questionData);
-    $correctAnswers = 0;
-
-    foreach ($_POST as $key => $answer){
-        if(is_numeric(substr($key,0,1)) && $answer){
-            $answerParts = explode('-', $key); // [1,1]
-            $questionId = $answerParts[0];
-            $choiceId = $answerParts[1];
-            $connection->query('INSERT INTO result_answer SET result_id = ' . $newResultId .', choice_id = ' . $choiceId);
-
-            // correct?
-            $answerKey = $DB->readData('SELECT is_correct FROM choice WHERE id = '. $choiceId);
-            if($answerKey[0]['is_correct']){
-                $correctAnswers++;
-            }
-
-        }
-    }
-
-    $score = $correctAnswers / $totalQuestions * 100;
-
-    print_r('YOUR SCORE: '. $score . '%');
-
-    /*foreach ($testQuestions as $single){
-        $single['question']['id']
-        $single['choices'][...['id']]
-    }*/
-
-    // evaluate results
-}
